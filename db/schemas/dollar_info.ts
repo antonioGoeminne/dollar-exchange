e984221fa1e78ca04944b68d352c923661b62fca
antonioGoeminne <antonionicolasgoeminne@gmail.com>
2026-03-03 08:16:22 -0300

    chore: adding husky

--- db/schemas/dollar_info.ts
+++ db/schemas/dollar_info.ts
@@ -5,14 +5,16 @@ import {
   pgTable,
   timestamp,
   varchar,
-} from "drizzle-orm/pg-core"
+} from "drizzle-orm/pg-core";
 
 export const dollarInfo = pgTable("dollar_info", {
   id: bigserial("id", { mode: "number" }).primaryKey(),
-  createdAt: timestamp("created_at", { withTimezone: true }).defaultNow().notNull(),
+  createdAt: timestamp("created_at", { withTimezone: true })
+    .defaultNow()
+    .notNull(),
   exchangeRate: numeric("exchange_rate", { precision: 18, scale: 4 }),
   date: date("date"),
   name: varchar("name", { length: 255 }),
   purchasePrice: numeric("purchase_price", { precision: 18, scale: 4 }),
   salePrice: numeric("sale_price", { precision: 18, scale: 4 }),
-})
+});
--- features/currency/api/get-dollar-prices.ts
+++ features/currency/api/get-dollar-prices.ts
@@ -2,6 +2,6 @@ import { dollarInfo } from "@/db/schemas/dollar_info";
 import { db } from "@/lib/db";
 
 export async function getDollarPrices() {
-    const dollarPrices = await db.select().from(dollarInfo);
-    return dollarPrices;
+  const dollarPrices = await db.select().from(dollarInfo);
+  return dollarPrices;
 }
