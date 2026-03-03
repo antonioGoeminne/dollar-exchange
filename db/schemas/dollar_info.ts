f7bd3b46d06c18130ccdcd12e7ae27e214f723bb
Toni <85411437+antonioGoeminne@users.noreply.github.com>
2026-03-03 20:25:40 -0300

    1 init config (#3)
    * feat: add database schema for dollar information and historical data; implement API to fetch dollar prices and add drizzle orm
    
    * chore: adding husky
    
    * chore: adding ci process to github actions and cleaning readme
    
    * chore: add db variable on yml to get from github servers

---
+++ db/schemas/dollar_info.ts
@@ -0,0 +1,20 @@
+import {
+  bigserial,
+  date,
+  numeric,
+  pgTable,
+  timestamp,
+  varchar,
+} from "drizzle-orm/pg-core";
+
+export const dollarInfo = pgTable("dollar_info", {
+  id: bigserial("id", { mode: "number" }).primaryKey(),
+  createdAt: timestamp("created_at", { withTimezone: true })
+    .defaultNow()
+    .notNull(),
+  exchangeRate: numeric("exchange_rate", { precision: 18, scale: 4 }),
+  date: date("date"),
+  name: varchar("name", { length: 255 }),
+  purchasePrice: numeric("purchase_price", { precision: 18, scale: 4 }),
+  salePrice: numeric("sale_price", { precision: 18, scale: 4 }),
+});
---
+++ features/currency/api/get-dollar-prices.ts
@@ -0,0 +1,7 @@
+import { dollarInfo } from "@/db/schemas/dollar_info";
+import { db } from "@/lib/db";
+
+export async function getDollarPrices() {
+  const dollarPrices = await db.select().from(dollarInfo);
+  return dollarPrices;
+}
