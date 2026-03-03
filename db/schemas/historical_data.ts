49faadadda4c54ca5bea2a09f3868d8bd35ec6f3
antonioGoeminne <antonionicolasgoeminne@gmail.com>
2026-03-03 08:12:03 -0300

    feat: add database schema for dollar information and historical data; implement API to fetch dollar prices and add drizzle orm

---
+++ db/schemas/historical_data.ts
@@ -0,0 +1,22 @@
+import {
+    bigint,
+    bigserial,
+    numeric,
+    pgTable,
+    timestamp,
+} from "drizzle-orm/pg-core"
+import { dollarInfo } from "./dollar_info"
+
+
+export const historicalData = pgTable(
+    "historical_Data",
+    {
+        id: bigserial("id", { mode: "number" }).primaryKey(),
+        createdAt: timestamp("created_at", { withTimezone: true }).defaultNow().notNull(),
+        dollarInfoId: bigint("dollar_info_id", { mode: "number" }).references(
+            () => dollarInfo.id
+        ),
+        salesPrice: numeric("sales_price", { precision: 18, scale: 4 }),
+        purchasePrice: numeric("purchase_price", { precision: 18, scale: 4 }),
+    }
+)
